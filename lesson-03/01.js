1e5d5a0abc13a8dcf3cf074055726d7724e2d0db
cubik707 <stdemidovez@gmail.com>
2024-02-10 21:28:41 +0300

    урок3-1 исправленный

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(color => color === "черный" || color === "красный" || color === "желтый").join('-')
+}
+
+console.log(createColorString());
+
